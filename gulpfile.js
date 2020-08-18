bdf79bdd7ec2324dd54fa3553be590fb274f7d19
Vsevolod425 <glasskitee@gmail.com>
2020-08-18 16:20:40 +0300

    training 1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,89 @@
-// Include gulp
-var gulp = require('gulp'),
-  // Include Our Plugins
-  less = require('gulp-less'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+'use strict';
 
-gulp.task('css', function () {
-  gulp
-    .src('./less/main.less')
-    .pipe(less({ strictMath: true }))
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var less = require("gulp-less");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+var server = require("browser-sync").create();
+
+gulp.task("css", function () {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
     .pipe(postcss([
-      autoprefixer({ browsers: ['> 1%', 'IE 9', 'IE 10']})
+      autoprefixer()
     ]))
-    .pipe(gulp.dest('./css'));
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./less/**/*.less', ['css']);
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
 });
 
-gulp.task('default', ['css', 'watch']);
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"))
+    .pipe(server.stream());
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+});
+
+gulp.watch("source/less/**/*.less", gulp.series("css"));
+gulp.watch("source/*.html", gulp.series("html"));
+});
+
+gulp.task("build", gulp.series(
+"clean",
+"copy",
+"css",
+"images",
+"webp",
+"sprite",
+"html"
+));
+
+gulp.task("start", gulp.series("build", "server"));
